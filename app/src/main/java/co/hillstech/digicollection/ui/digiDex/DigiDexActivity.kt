2f3e70a553523f3423cd433892546aafd5fcc4c3
Manoel Afonso <mano.afonso93@gmail.com>
2019-07-02 18:19:16 -0300

    Adding DigiDex ActionBar Title

--- app/src/main/java/co/hillstech/digicollection/ui/digiDex/DigiDexActivity.kt
+++ app/src/main/java/co/hillstech/digicollection/ui/digiDex/DigiDexActivity.kt
@@ -1,12 +1,15 @@
 package co.hillstech.digicollection.ui.digiDex
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import co.hillstech.digicollection.R
+import co.hillstech.digicollection.Session
 import co.hillstech.digicollection.activities.bases.BaseActivity
 import co.hillstech.digicollection.adapters.EdgeDecorator
 import kotlinx.android.synthetic.main.activity_digi_dex.*
+import kotlinx.android.synthetic.main.view_action_bar.*
 
 class DigiDexActivity : BaseActivity(), DigiDexPresenter.View {
 
@@ -18,12 +21,25 @@ class DigiDexActivity : BaseActivity(), DigiDexPresenter.View {
 
         presenter.view = this
 
+        setupActionBar()
+        setupViews()
+
         presenter.requestMonsterList()
+    }
+
+    override fun setupActionBar() {
+        viewActivityTitle.text = getString(R.string.digidex)
+
+        viewBackArrow.setOnClickListener { onBackPressed() }
+
+        Session.user?.crest?.color.let {
+            setStatusBarColor(it)
+            viewActionBar.setCardBackgroundColor(Color.parseColor(it))
+        }
     }
 
     override fun inflateMonsterList() {
         viewMonsterList?.apply {
-            addItemDecoration(EdgeDecorator(16))
             adapter = DigiDexAdapter(presenter.getMonsterList(), {})
             layoutManager = GridLayoutManager(this@DigiDexActivity, 3)
         }
