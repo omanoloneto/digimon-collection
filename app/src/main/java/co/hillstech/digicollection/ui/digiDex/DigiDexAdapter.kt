998d79049699bbb901533ad356b42d14e8c14258
Manoel Afonso <mano.afonso93@gmail.com>
2019-07-03 18:07:27 -0300

    Removing saturation in monsters not been seen

--- app/src/main/java/co/hillstech/digicollection/ui/digiDex/DigiDexAdapter.kt
+++ app/src/main/java/co/hillstech/digicollection/ui/digiDex/DigiDexAdapter.kt
@@ -1,5 +1,8 @@
 package co.hillstech.digicollection.ui.digiDex
 
+import android.graphics.ColorFilter
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +12,8 @@ import co.hillstech.digicollection.models.Monster
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.view_digidex_item.view.*
 import kotlinx.android.synthetic.main.view_digivice_list_items.view.viewImage
+
+
 
 class DigiDexAdapter(private val items: List<Monster>,
                      private val onMonsterClick: () -> Unit) : RecyclerView.Adapter<DigiDexHolder>() {
@@ -33,17 +38,24 @@ class DigiDexHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bindView(item: Monster, onMonsterClick: () -> Unit) = with(itemView) {
         viewSpecies.text = item.species
 
+        val matrix = ColorMatrix()
+
         if (item.scanned) {
             viewImage.alpha = 1f
+            matrix.setSaturation(1f)
+            layoutMonster.setOnClickListener { onMonsterClick() }
         } else {
             viewImage.alpha = 0.1f
+            matrix.setSaturation(0f)
+            layoutMonster.setOnClickListener { }
         }
+
+        viewImage.colorFilter = ColorMatrixColorFilter(matrix)
 
         Picasso.get().load(item.image)
                 .placeholder(R.drawable.placeholder)
                 .into(viewImage)
 
-        layoutMonster.setOnClickListener { onMonsterClick() }
     }
 
 }
